e6eb6c83fb0af447e8daeeeb3fa4b256e9297f44
Oguz Yuksel <oguz.yueksel@teufel.de>
2023-06-20 16:02:23 +0200

    modernize implementation

--- Sources/LottieExtensions/Extensions/SwiftUI/View.swift
+++ Sources/LottieExtensions/Extensions/SwiftUI/View.swift
@@ -1,8 +1,30 @@
-//
-//  File.swift
-//  
-//
-//  Created by Oguz Yüksel on 19.06.23.
-//
+// Copyright © 2023 Lautsprecher Teufel GmbH. All rights reserved.
 
-import Foundation
+import Lottie
+import SwiftUI
+
+extension View {
+    /// Applies a configuration to the `LottieAnimationView` in the environment.
+    ///
+    /// Use this modifier to provide a `LottieAnimationView` instance with specific properties that will be
+    /// used as a configuration in the environment. The configuration will be applied when a `LottieView` instance
+    /// is created or updated in the context of this view.
+    ///
+    /// - Parameter lottieAnimationView: The `LottieAnimationView` instance that contains the configuration to be applied.
+    /// - Returns: A modified version of the view with the new `LottieAnimationView` configuration.
+    ///
+    /// Example:
+    /// ```swift
+    /// LottieView(LottieAnimationView(name: "animation"))
+    ///     .lottieAnimationConfiguration {
+    ///           $0.loopMode = .playOnce
+    ///      }
+    /// ```
+    ///
+    @ViewBuilder
+    public func lottieAnimationConfiguration(
+        _ lottieAnimationViewClosure: @escaping (LottieAnimationView) -> Void
+    ) -> some View {
+        environment(\.lottieAnimationConfiguration, lottieAnimationViewClosure)
+    }
+}
--- Sources/LottieExtensions/LottieView.swift
+++ Sources/LottieExtensions/LottieView.swift
@@ -1,53 +1,132 @@
-//
-//  LottieView.swift
-//  UIExtensions
-//
-//  Created by Luiz Barbosa on 15.01.20.
-//  Copyright © 2020 Lautsprecher Teufel GmbH. All rights reserved.
-//
+// Copyright © 2023 Lautsprecher Teufel GmbH. All rights reserved.
 
-import Foundation
 import Lottie
 import SwiftUI
-import UIExtensions
+import struct UIExtensions.DisableAnimationsEnvironmentKey
 
+
+
+/// `LottieView` is a struct that adapts `LottieAnimationView` to `UIViewRepresentable` protocol.
+/// It enables using Lottie animations in SwiftUI.
 public struct LottieView: UIViewRepresentable {
-    let animationView = AnimationView()
-    let filename: String
-    let bundle: Bundle
-    let loopMode: LottieLoopMode
-    let completion: LottieCompletionBlock?
-    @Environment(\.lottieAnimationProgress) var lottieAnimationProgress
+    public typealias Configuration = LottieAnimationView
+    private let makeAnimationView: () -> LottieAnimationView
 
-    public init(filename: String, bundle: Bundle, loopMode: LottieLoopMode = .playOnce, completion: LottieCompletionBlock? = nil) {
-        self.filename = filename
-        self.bundle = bundle
-        self.loopMode = loopMode
-        self.completion = completion
+    // Deprecated
+    private let deprecatedCompletion: LottieCompletionBlock?
+    private let runDeprecatedLogic: ((UIView, Context) -> Void)?
+
+    /// Initializes a new `LottieView` with a closure that creates an instance of `LottieAnimationView`.
+    ///
+    /// - Parameter animationView: A closure that creates a `LottieAnimationView` instance.
+    ///
+    /// Example:
+    /// ```swift
+    /// LottieView(Configuration(name: animationFile.name, bundle: animationFile.bundle))
+    /// ```
+    public init(
+        _ animationView: @escaping @autoclosure () -> Configuration
+    ) {
+        self.makeAnimationView = animationView
+        self.deprecatedCompletion = nil
+        self.runDeprecatedLogic = nil
     }
 
-    public func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
-        let view = UIView()
-        let animation = Animation.named(filename, bundle: bundle)
-        animationView.animation = animation
-        animationView.loopMode = loopMode
-        animationView.contentMode = .scaleAspectFit
-        animationView.backgroundBehavior = .pauseAndRestore
-        if context.environment.disableAnimations {
-            animationView.currentProgress = lottieAnimationProgress
-        } else {
-            animationView.play(completion: completion)
-        }
+    public func makeUIView(context: Context) -> UIView {
+        let animationView = makeAnimationView()
+        let wrapperView = UIView()
+
         animationView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(animationView)
-
+        wrapperView.addSubview(animationView)
         NSLayoutConstraint.activate([
-            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
-            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+            animationView.heightAnchor.constraint(equalTo: wrapperView.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: wrapperView.widthAnchor)
         ])
-        return view
+
+        runDeprecatedLogic?(wrapperView, context)
+        return wrapperView
     }
 
-    public func updateUIView(_ uiView: LottieView.UIViewType, context: UIViewRepresentableContext<LottieView>) {
+    public func updateUIView(_ uiView: LottieView.UIViewType, context: Context) {
+        context.environment.lottieAnimationConfiguration(Self.animationView(uiView))
+        runDeprecatedLogic?(uiView, context)
+    }
+}
+
+// MARK: - Helpers
+extension LottieView {
+    static private func animationView(_ wrapperView: UIView) -> LottieAnimationView {
+        guard let animationView = wrapperView.subviews.first as? LottieAnimationView
+        else {
+            preconditionFailure(
+                """
+                Wrapper view should be initilized before this method called.
+                """
+            )
+        }
+        return animationView
+    }
+}
+
+// MARK: - Configurations
+// MARK: LottieAnimationConfiguration
+public struct LottieAnimationConfigurationKey: EnvironmentKey {
+    public static var defaultValue: (LottieAnimationView) -> Void = {
+        $0.loopMode = .playOnce
+        $0.contentMode = .scaleAspectFit
+        $0.backgroundBehavior = .pauseAndRestore
+    }
+}
+
+extension EnvironmentValues {
+    public var lottieAnimationConfiguration: (LottieAnimationView) -> Void {
+        get { self[LottieAnimationConfigurationKey.self] }
+        set { self[LottieAnimationConfigurationKey.self] = newValue }
+    }
+}
+
+// MARK: LottieAnimationProgress: Deprecated
+extension EnvironmentValues {
+    /// Deprecated
+    /// The progress of the Lottie animation.
+    @available(*, deprecated, message: "This property is deprecated. Please use `lottieAnimationConfiguration` to configure and control Lottie animations.")
+    public var lottieAnimationProgress: CGFloat {
+        get { return self[LottieAnimationProgressEnvironmentKey.self] }
+        set { self[LottieAnimationProgressEnvironmentKey.self] = newValue }
+    }
+}
+
+public struct LottieAnimationProgressEnvironmentKey: EnvironmentKey {
+    public static let defaultValue: CGFloat = 0.5
+}
+
+// MARK: - Deprecated
+extension LottieView {
+    /// Deprecated initializer
+    /// - Parameters:
+    ///   - filename: Name of the Lottie animation file.
+    ///   - bundle: Bundle containing the Lottie animation file.
+    ///   - loopMode: Animation loop mode. Defaults to `.playOnce`.
+    ///   - completion: Completion block to be executed when animation finishes. Defaults to `nil`.
+    /// - Returns: An instance of `LottieView`.
+    @available(*, deprecated, message: "This initializer is deprecated. Please use `init(_ animationView: LottieAnimationView)` instead.")
+    @_disfavoredOverload
+    public init(
+        filename: String,
+        bundle: Bundle,
+        loopMode: LottieLoopMode = .playOnce,
+        completion: LottieCompletionBlock? = nil
+    ) {
+        let animationView = LottieAnimationView(name: filename, bundle: bundle)
+        animationView.loopMode = loopMode
+        self.makeAnimationView = { animationView }
+        self.deprecatedCompletion = completion
+        self.runDeprecatedLogic = { view, context in
+            if context.environment.disableAnimations {
+                LottieView.animationView(view).currentProgress = context.environment.lottieAnimationProgress
+            } else {
+                LottieView.animationView(view).play(completion: completion)
+            }
+        }
     }
 }
