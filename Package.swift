9848204984248e85a61a833d5dcc043ac9c0cc93
Oguz Yuksel <oguz.yueksel@teufel.de>
2023-08-01 11:57:35 +0200

    dependency version update

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Lottie", url: "https://github.com/airbnb/lottie-ios.git", from: "4.2.0"),
-        .package(name: "UIExtensions", url: "https://github.com/teufelaudio/UIExtensions.git", from: "0.7.0")
+        .package(name: "UIExtensions", url: "https://github.com/teufelaudio/UIExtensions.git", from: "1.0.0")
     ],
     targets: [
         .target(name: "LottieExtensions", dependencies: ["Lottie", "UIExtensions"]),
