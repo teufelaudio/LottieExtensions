a99de206dd92184aa22accdd33c1d5a31f6de5e3
Lukas Liebl <Lukas.Liebl@Teufel.de>
2024-10-25 14:52:55 +0200

    Updates UIExtension to 3.0.0

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.4.0"),
-        .package(url: "https://github.com/teufelaudio/UIExtensions.git", from: "2.1.0")
+        .package(url: "https://github.com/teufelaudio/UIExtensions.git", from: "3.0.0")
     ],
     targets: [
         .target(
